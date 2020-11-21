98bcf748c33f4276ea630f34062bcb11248efb9c
RishaanvB <69298949+RishaanvB@users.noreply.github.com>
2020-11-21 22:00:37 +0100

    Add files via upload
    laatste commit met bonusopdrachten

--- farm.js
+++ farm.js
@@ -1,11 +1,47 @@
-const getYieldForPlant = function (crop) {
-    return crop.yield
+const getYieldForPlant = (crop, factor) => {
+    if (factor === undefined) {
+        let totalYield = crop.yield
+        return totalYield
+    }
+    else {
+        let growthFactorArray = []
+        // calculates growthfactor for each of the given external factors in factor parameter, puts in array
+        for (const [key, value] of Object.entries(factor)) {
+            growthFactorArray.push((crop.factors[key][value] + 100) / 100)
+        }
+        const reducer = (acc, val) => acc * val;
+        growthFactor = growthFactorArray.reduce(reducer)
+        let plantYield = crop.yield
+        let totalYield = plantYield * growthFactor
+        return totalYield
+    }
 }
 
-const getYieldForCrop = (crop) => {
+
+
+
+
+const getYieldForCrop = (crop, factor) => {
     let plantYield = crop.crop.yield
     let cropAmount = crop.numCrops;
-    return (plantYield * cropAmount)
+    let totalYield = plantYield * cropAmount
+    if (factor === undefined) {
+        // console.log("factor is undefined"); //<--Why is this logging multiple times?
+        return totalYield
+    }
+    else {
+        let growthFactorArray = []
+        // calculates growthfactor for each of the given external factors in factor parameter, puts in array
+        for (const [key, value] of Object.entries(factor)) {
+            growthFactorArray.push((crop.crop.factors[key][value] + 100) / 100)
+        }
+        const reducer = (acc, val) => acc * val;
+        // multiplies array of growthfactor and multiplies by plantYield
+        growthFactor = growthFactorArray.reduce(reducer)
+        let totalYieldforCrop = totalYield * growthFactor
+        return totalYieldforCrop
+    }
+
 }
 
 
@@ -31,29 +67,40 @@ const getCostsForCrop = (input) => {
 
 
 
-const getRevenueForCrop = (input) => {
-    let salePrice = input.crop.salePrice
+const getRevenueForCrop = (input, factor) => {
     let plantYield = input.crop.yield
+    let salePrice = input.crop.salePrice
     let cropAmount = input.numCrops
-    let totalYield = (plantYield * cropAmount)
-    let revenue = (salePrice * totalYield)
-    return revenue
 
+    if (factor === undefined) {
+        let totalYield = (plantYield * cropAmount)
+        let revenue = (salePrice * totalYield)
+        return revenue
+    }
+    else {
+        let growthFactorArray = []
+        for (const [key, value] of Object.entries(factor)) {
+            growthFactorArray.push((input.crop.factors[key][value] + 100) / 100)
+        }
+        const reducer = (acc, val) => acc * val;
+        growthFactor = growthFactorArray.reduce(reducer)
+        let totalYield = (plantYield * cropAmount * growthFactor)
+        let revenue = (salePrice * totalYield)
+        return revenue
+    }
 }
 
-
-const getProfitForCrop = (input) => {
-    let profit = getRevenueForCrop(input) - getCostsForCrop(input)
+// external factors do NOT  affect getCostsforCrop
+const getProfitForCrop = (input, factor) => {
+    let profit = getRevenueForCrop(input, factor) - getCostsForCrop(input)
     return profit
 
 }
 
-// bereken de winst voor meerdere crops (zonder omgevingsfactoren): getTotalProfit
-
-const getTotalProfit = ({ crops }) => {
+const getTotalProfit = ({ crops }, factor) => {
     let newArray = []
     crops.forEach(crop => {
-        newArray.push(getProfitForCrop(crop))
+        newArray.push(getProfitForCrop(crop, factor))
     });
     const reducer = (acc, val) => acc + val;
     let totalYield = newArray.reduce(reducer)
@@ -68,3 +115,4 @@ module.exports = {
     getProfitForCrop: getProfitForCrop,
     getTotalProfit: getTotalProfit,
 }
+
--- farm.test.js
+++ farm.test.js
@@ -15,10 +15,53 @@ describe("getYieldForPlant", () => {
     const corn = {
         name: "corn",
         yield: 30,
+        factors: {
+            sun: {
+                low: -50,
+                medium: 0,
+                high: 50,
+            },
+            wind: {
+                low: 10,
+                medium: 50,
+                high: 100,
+            },
+            rain: {
+                low: 25,
+                medium: -70,
+                high: 30,
+            },
+        },
     };
+
+
 
     test("Get yield for plant with no environment factors", () => {
         expect(getYieldForPlant(corn)).toBe(30);
+    });
+
+
+
+    test("Get yield for plant with external sun/wind factors", () => {
+        const environmentFactorsA = { //yield is 30
+            sun: "high", //50
+            wind: "high", //100
+            // 30 * 1.5* 2 = 90
+        };
+        const environmentFactorsB = {
+            sun: "low", //-50
+            wind: "low", //10
+            //30 *0.5*1.1 = 16.5
+        };
+        const environmentFactorsC = {
+            sun: "medium", //0
+            wind: "low", //10
+            rain: "medium" //-70
+            //30*1*1.1*0.3 = 9.9
+        };
+        expect(getYieldForPlant(corn, environmentFactorsA)).toBe(90);
+        expect(getYieldForPlant(corn, environmentFactorsB)).toBe(16.5);
+        expect(getYieldForPlant(corn, environmentFactorsC)).toBe(9.9);
     });
 });
 
@@ -34,6 +77,39 @@ describe("getYieldForCrop", () => {
         };
         expect(getYieldForCrop(input)).toBe(30);
     });
+
+    test('Calculate yield for single crop with external factors', () => {
+        const corn = {
+            name: "corn",
+            yield: 3,
+            factors: {
+                sun: {
+                    low: -50,
+                    medium: 0,
+                    high: 50,
+                },
+                wind: {
+                    low: 10,
+                    medium: 50,
+                    high: 100,
+                },
+            },
+        };
+        const input = {
+            crop: corn,
+            numCrops: 10,
+        };
+        const environmentFactors = {
+            sun: "high",
+            wind: "high",
+
+        };
+
+        expect(getYieldForCrop(input, environmentFactors)).toBe(90);
+
+    });
+
+
 });
 
 describe("getTotalYield", () => {
@@ -78,6 +154,7 @@ describe('getCostsForCrop ', () => {
         expect(getCostsForCrop(input)).toBe(50);
 
     });
+
 });
 
 
@@ -95,6 +172,39 @@ describe('getRevenueForCrop ', () => {
         };
 
         expect(getRevenueForCrop(input)).toBe(60);
+
+    });
+    test('Calculate revenue for crop with external factors', () => {
+        // revenue  = salePrice * totalyield *   numCrops = 2*3 *3 * 10 = 180
+        // revenue is getRevenueForCrop * growthfactor
+        const corn = {
+            name: "corn",
+            yield: 3,
+            salePrice: 2,
+            factors: {
+                sun: {
+                    low: -50,
+                    medium: 0,
+                    high: 50,
+                },
+                wind: {
+                    low: 10,
+                    medium: 50,
+                    high: 100,
+                },
+            },
+        };
+        const input = {
+            crop: corn,
+            numCrops: 10,
+        };
+        const environmentFactors = {
+            sun: "high",
+            wind: "high",
+
+        };
+        expect(getRevenueForCrop(input)).toBe(60);
+        expect(getRevenueForCrop(input, environmentFactors)).toBe(180);
 
     });
 });
@@ -116,6 +226,40 @@ describe('getProfitForCrop ', () => {
         };
 
         expect(getProfitForCrop(input)).toBe(10);
+
+    });
+    test('Calculate profit for crop with external factors', () => {
+        // revenue  = salePrice * yield(with factor) * numCrops = 2 * 9 * 10 =180
+        // costs = seedCosts * numCrops = 5 * numCrops = 5 * 10 = 50
+        // profit = revenue -costs = 180 -50 =130
+        const corn = {
+            name: "corn",
+            yield: 3,
+            factors: {
+                sun: {
+                    low: -50,
+                    medium: 0,
+                    high: 50,
+                },
+                wind: {
+                    low: 10,
+                    medium: 50,
+                    high: 100,
+                },
+            },
+            salePrice: 2,
+            seedCosts: 5,
+        };
+        const input = {
+            crop: corn,
+            numCrops: 10,
+        };
+        const environmentFactors = {
+            sun: "high",
+            wind: "high",
+
+        };
+        expect(getProfitForCrop(input, environmentFactors)).toBe(130);
 
     });
 });
@@ -154,5 +298,68 @@ describe("getTotalProfit", () => {
         expect(getTotalProfit({ crops })).toBe(41);
     });
 
+    test("Calculate total profit with multiple crops AND with external factors", () => {
+        const corn = {
+            name: "corn",
+            yield: 3,
+            factors: {
+                sun: {
+                    low: -50,
+                    medium: 0,
+                    high: 50,
+                },
+                wind: {
+                    low: 10,
+                    medium: 50,
+                    high: 100,
+                },
+            },
+            salePrice: 2,
+            seedCosts: 5,
+        };
+
+        const pumpkin = {
+            name: "pumpkin",
+            yield: 4,
+            factors: {
+                sun: {
+                    low: -20,
+                    medium: 25,
+                    high: 40,
+                },
+                wind: {
+                    low: -25,
+                    medium: 60,
+                    high: 80,
+                },
+            },
+            salePrice: 5,
+            seedCosts: 2,
+        };
+        const crops = [
+            { crop: corn, numCrops: 5 },
+            { crop: pumpkin, numCrops: 2 },
+        ];
+
+        const environmentFactors = {
+            sun: "high",
+            wind: "high",
+
+        };
+        // ====CORN====  
+        // revenue  = salePrice * yield(with factor) * numCrops = 2 * 9 * 5 =90
+        // costs = seedCosts * numCrops = 5 * numCrops = 5 * 5 = 25
+        // profit = revenue -costs = 90 -25 =65
+
+
+        // ====PUMPKIN====  
+        // revenue  = salePrice  * yield(with factor) * numCrops = 5 * 10.08 * 2 = 100.8
+        // costs = seedCosts * numCrops =  2 * 2 = 4
+        // profit = revenue -costs = 100.8 - 4 =96.8
+
+        expect(getProfitForCrop(crops[0], environmentFactors)).toBe(65);
+        expect(getProfitForCrop(crops[1], environmentFactors)).toBe(96.8);
+        expect(getTotalProfit({ crops }, environmentFactors)).toBe(161.8);
+    });
 
 });
